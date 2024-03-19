6a1f29d8d42406be68ba22f102380aab3d1b98ba
gokberkkaya <kygokberk@gmail.com>
2024-03-19 04:08:18 +0300

    database connection, models struct, detail page etc. added

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require('./models/Photo');
+
 const app = express();
-const port = 3000;
+
+mongoose.connect('mongodb://localhost/share-photo-db');
 
 // template engine
 app.set('view engine', 'ejs');
 
 // middleware
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/', async (req, res) => {
+    const photos = await Photo.find({});
+
+    res.render('index', {
+        photos
+    });
 });
 
 app.get('/about', (req, res) => {
@@ -22,6 +32,21 @@ app.get('/add', (req, res) => {
     res.render('add');
 });
 
+app.get('/photos/:id', async (req, res) => {
+    const photo = await Photo.findById(req.params.id);
+
+    res.render('photo', {
+        photo
+    });
+});
+
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body);
+
+    res.redirect('/');
+});
+
+const port = 3000;
 app.listen(port, () => {
     console.log(`Connected to ${ port } port!`);
 });
